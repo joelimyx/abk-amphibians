4c30b00e8a2942dde33fca0a5d48a692e95175c8
Joe Lim <joelimyx@gmail.com>
2022-03-18 11:40:27 -0400

    initial commit

---
+++ app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.amphibians.ui
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.amphibians.network.Amphibian
+import com.example.amphibians.network.AmphibianApi
+import kotlinx.coroutines.launch
+
+enum class AmphibianApiStatus {LOADING, ERROR, DONE}
+
+class AmphibianViewModel : ViewModel() {
+
+    private var _status = MutableLiveData<AmphibianApiStatus>()
+    val status: LiveData<AmphibianApiStatus> = _status
+
+    private var _amphibianList = MutableLiveData<List<Amphibian>>()
+    val amphibianList: LiveData<List<Amphibian>> = _amphibianList
+
+    private var _amphibian = MutableLiveData<Amphibian>()
+    val amphibian: LiveData<Amphibian> = _amphibian
+
+    fun getAmphibianFromApi(){
+        viewModelScope.launch {
+            _status.value = AmphibianApiStatus.LOADING
+            try {
+                _amphibianList.value = AmphibianApi.retrofitService.getList()
+                _status.value = AmphibianApiStatus.DONE
+            }catch (e: Error){
+                Log.e("CoroutineScope", "Cannot get")
+                _status.value = AmphibianApiStatus.ERROR
+                _amphibianList.value = listOf()
+            }
+
+        }
+    }
+
+    fun onAmphibianClicked(amphibian: Amphibian) {
+
+        _amphibian.value = amphibian
+    }
+}
